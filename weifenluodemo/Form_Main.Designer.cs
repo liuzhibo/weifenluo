1679ab1c5cafd4166484efe510471ee82b2c6d0e
liuzhibo <liuzhibo@vip.qq.com>
2019-04-04 10:23:10 +0800

    添加项目文件。

---
+++ weifenluodemo/Form_Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace weifenluodemo
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dockPanel1 = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // dockPanel1
+            // 
+            this.dockPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel1.Location = new System.Drawing.Point(0, 24);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.Size = new System.Drawing.Size(800, 426);
+            this.dockPanel1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dockPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form_Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ weifenluodemo/Form_Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace weifenluodemo
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            dockPanel1.AllowEndUserDocking = false;
+
+            Form_Bottom form_Bottom = new Form_Bottom();
+            Form_Left form_Left = new Form_Left();
+            Form_Right form_Right = new Form_Right();
+            Form_Center form_Center = new Form_Center();
+            Form_Top form_Top = new Form_Top();
+
+            form_Bottom.CloseButtonVisible = false;
+            form_Left.CloseButtonVisible = false;
+            form_Right.CloseButtonVisible = false;
+            form_Center.CloseButtonVisible = false;
+            form_Top.CloseButtonVisible = false;
+
+            form_Bottom.Show(this.dockPanel1, DockState.DockBottom);
+            form_Left.Show(this.dockPanel1, DockState.DockLeft);
+            form_Right.Show(this.dockPanel1, DockState.DockRight);
+            form_Top.Show(this.dockPanel1,DockState.DockTop);
+            form_Center.Show(this.dockPanel1);
+        }
+
+    }
+}
